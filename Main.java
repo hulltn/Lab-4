f8b50cf8358e568694ec21426f98ba181972e469
hulltn <hulltn@mail.uc.edu>
2020-09-16 17:41:44 +0000

    For statements, if statements

--- Main.java
+++ Main.java
@@ -1,7 +1,26 @@
 import java.util.Random;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
     Random r = new Random();
+    int randomNum = r.nextInt(50) + 1;
+    System.out.println("The random number is: " + randomNum);
+    if (randomNum > 0)
+      for (int count = randomNum; count >= 0; count--) 
+      {
+        System.out.println(count);
+      }
+    if (randomNum < 25)
+    {
+      System.out.println("Ahoy mateys!");
+    }
+    else if (randomNum > 25 & randomNum < 42)
+    {
+      System.out.println("Cannonball!");
+    }
+    else 
+    {
+      System.out.println("Blast Off!");
+    }
   }
 }
